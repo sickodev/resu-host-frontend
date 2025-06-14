78d962c0b4fac2616490d2c14b4d524ad09e093a
sickodev <kbishwa03kalyan@gmail.com>
2025-06-14 12:38:20 +0530

    adds endpoint to upload files

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,8 @@
 // place files you want to import through the `$lib` alias in this folder.
+type EnvironmentVariables = {
+	BACKEND_URL: string;
+}
+
+export const env: EnvironmentVariables = {
+	BACKEND_URL: import.meta.env.VITE_PUBLIC_BACKEND_URL,
+}
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import H2 from '../../components/ui/h2.svelte';
-	import H4 from '../../components/ui/h4.svelte';
+	import { env } from '$lib';
 
 	let file: File | null = null;
 	let uploading: boolean = false;
@@ -25,18 +25,35 @@
 		successLink = '';
 
 		try {
-			await new Promise((r) => setTimeout(r, 1000)); // Simulated upload delay
-			successLink = 'https://resu.host/fake-link';
+			const formData = new FormData();
+			formData.append('file', file);
+
+			const response = await fetch(`${env.BACKEND_URL}/upload`,{
+				method: 'POST',
+				body: formData
+			});
+
+			if(!response.ok){
+				const errorData = await response.json();
+				errorMessage = errorData.message || 'Upload Failed'
+				return;
+			}
+
+			const result = await response.json();
+			console.log(result)
+			successLink = result.data.filePath;
 		} catch (error: any) {
 			errorMessage = error.message();
 		}finally{
             uploading = false;
-        }
+		}
 	}
 </script>
 
 <div class="mx-auto space-y-6 rounded-lg bg-neutral-800 p-6 shadow-lg filter">
 	<H2 className="text-neutral-100 text-center">Upload Your Resume</H2>
+	<p class="text-center text-neutral-600 font-semibold">Your upload self destructs in 15 days</p>
+	<hr class="border border-neutral-700"/>
 	<input
 		type="file"
 		accept=".pdf"
@@ -63,7 +80,7 @@
 <div class={`bg-neutral-800 w-full rounded-lg text-center font-bold ${successLink?"p-2":"p-0"}`}>
     {#if successLink}
     <div class="p-4 rounded text-sm">
-      <p class="text-green-400 mb-1">✅ Uploaded successfully!</p>
+      <p class="text-emerald-500 mb-1"> Uploaded successfully!</p>
       <a href={successLink} class="text-blue-400 underline break-all">{successLink}</a>
     </div>
   {/if}
