ad12be0418522b4406100383861f0ad2ef8a3a8f
sickodev <kbishwa03kalyan@gmail.com>
2025-06-21 23:54:40 +0530

    adds custom expiry filter

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -3,6 +3,7 @@
 	import { env } from '$lib';
 
 	let file: File | null = null;
+	let expiresIn: number = 15;
 	let uploading: boolean = false;
 	let errorMessage: string = '';
 	let successLink: string = '';
@@ -28,15 +29,16 @@
 		try {
 			const formData = new FormData();
 			formData.append('file', file);
+			formData.append('expiresIn', expiresIn.toString())
 
-			const response = await fetch(`${env.BACKEND_URL}/upload`,{
+			const response = await fetch(`${env.BACKEND_URL}/upload`, {
 				method: 'POST',
 				body: formData
 			});
 
-			if(!response.ok){
+			if (!response.ok) {
 				const errorData = await response.json();
-				errorMessage = errorData.message || 'Upload Failed'
+				errorMessage = errorData.message || 'Upload Failed';
 				return;
 			}
 
@@ -45,8 +47,8 @@
 			expiresAt = new Date(result.data.expiresAt).toLocaleString();
 		} catch (error: any) {
 			errorMessage = error.message();
-		}finally{
-            uploading = false;
+		} finally {
+			uploading = false;
 		}
 	}
 </script>
@@ -54,7 +56,7 @@
 <div class="mx-auto space-y-6 rounded-lg bg-neutral-800 p-6 shadow-lg filter">
 	<H2 className="text-neutral-100 text-center">Upload Your Resume</H2>
 	<p class="text-center text-neutral-600 font-semibold">Your upload self destructs in 15 days</p>
-	<hr class="border border-neutral-700"/>
+	<hr class="border border-neutral-700" />
 	<input
 		type="file"
 		accept=".pdf"
@@ -63,6 +65,14 @@
            text-neutral-300 file:cursor-pointer file:rounded file:border-0 file:bg-blue-600
            file:px-4 file:py-2 file:text-white"
 	/>
+	<div class="flex items-center space-x-2">
+		<label for="expiry" class="block mb-1 text-lg">Expires after:</label>
+		<select id="expiry" bind:value={expiresIn} class="bg-neutral-800 text-neutral-100 p-2 rounded w-2/3">
+			<option value="7">7 days</option>
+			<option value="15" selected>15 days</option>
+			<option value="30">30 days</option>
+		</select>
+	</div>
 
 	<button
 		on:click={handleUpload}
@@ -73,17 +83,17 @@
 	</button>
 </div>
 <div class={`bg-neutral-800 w-full rounded-lg text-center font-bold ${errorMessage?"p-2":"p-0"}`}>
-    {#if errorMessage}
+	{#if errorMessage}
 		<p class="text-sm text-red-500">{errorMessage}</p>
 	{/if}
 </div>
 
 <div class={`bg-neutral-800 w-full rounded-lg text-center font-bold ${successLink?"p-2":"p-0"}`}>
-    {#if successLink}
-    <div class="p-4 rounded text-sm">
-      <p class="text-emerald-500 mb-1"> Uploaded successfully!</p>
-      <a href={successLink} class="text-blue-400 underline break-all">{successLink}</a>
+	{#if successLink}
+		<div class="p-4 rounded text-sm">
+			<p class="text-emerald-500 mb-1"> Uploaded successfully!</p>
+			<a href={successLink} class="text-blue-400 underline break-all">{successLink}</a>
 			<p class="text-neutral-500 font-semibold">Expires At: {expiresAt}</p>
-    </div>
-  {/if}
+		</div>
+	{/if}
 </div>
